765a01563d55b16d907bcae1ad23b33c6dcfe271
DJAKOUA KWANKAM BRAYAN STEVE <brayan@example.com>
2026-06-22 01:50:41 +0100

    fix: allow supabase storage host in next.config.ts for avatars

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,15 @@ const nextConfig: NextConfig = {
         hostname: "image.tmdb.org",
         pathname: "/t/p/**",
       },
+      {
+        protocol: "https",
+        hostname: "qcykyywyvwmcixqunvto.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      }
     ],
   },
 };
